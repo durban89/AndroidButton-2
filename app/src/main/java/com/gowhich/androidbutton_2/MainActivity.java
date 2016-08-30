c352fccc84f03c61aadf9f7fa7d8cbbd4d2f1793
dapeng126 <zhangdapeng89@126.com>
2016-08-30 09:21:27 +0800

    按钮 图文混排

--- app/src/main/java/com/gowhich/androidbutton_2/MainActivity.java
+++ app/src/main/java/com/gowhich/androidbutton_2/MainActivity.java
@@ -1,13 +1,40 @@
 package com.gowhich.androidbutton_2;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.DynamicDrawableSpan;
+import android.text.style.ImageSpan;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) this.findViewById(R.id.button);
+
+        SpannableString spannableStringLeft = new SpannableString("left");
+
+        Bitmap bitmapLeft = BitmapFactory.decodeResource(getResources(), R.drawable.face5);
+        ImageSpan imageSpanLeft = new ImageSpan(bitmapLeft, DynamicDrawableSpan.ALIGN_BOTTOM);
+        spannableStringLeft.setSpan(imageSpanLeft, 0, 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        SpannableString spannableStringRight = new SpannableString("right");
+        Bitmap bitmapRight = BitmapFactory.decodeResource(getResources(), R.drawable.face6);
+        ImageSpan imageSpanRight = new ImageSpan(bitmapRight, DynamicDrawableSpan.ALIGN_BOTTOM);
+        spannableStringRight.setSpan(imageSpanRight, 0, 5, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        button.append(spannableStringLeft);
+        button.append("我的按钮");
+        button.append(spannableStringRight);
+
     }
 }
